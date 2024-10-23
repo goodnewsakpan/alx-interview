3d38d03430001c3bfc676ac66cf80132391ae5f2
goodnewsakpan1goodnewsakpan1goodnewsakpan <goodnewsakpan123@gmail.com>
2024-10-23 09:11:57 +0100

    adjusted 0-making_change.py file and added main.py file to test greed algorithm

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,31 +1,24 @@
 #!/usr/bin/python3
+""" using greedy algorithm to determine the
+    minimum number of coins
+"""
 
-""" Module for solving prime game question """
 
-
-def isWinner(x, nums):
-    """function that checks for the winner"""
-    if not nums or x < 1:
-        return None
-    max_num = max(nums)
-
-    my_filter = [True for _ in range(max(max_num + 1, 2))]
-    for i in range(2, int(pow(max_num, 0.5)) + 1):
-        if not my_filter[i]:
-            continue
-        for j in range(i * i, max_num + 1, i):
-            my_filter[j] = False
-    my_filter[0] = my_filter[1] = False
-    y = 0
-    for i in range(len(my_filter)):
-        if my_filter[i]:
-            y += 1
-        my_filter[i] = y
-    player1 = 0
-    for x in nums:
-        player1 += my_filter[x] % 2 == 1
-    if player1 * 2 == len(nums):
-        return None
-    if player1 * 2 > len(nums):
-        return "Maria"
-    return "Ben"
+def makeChange(coins, total):
+    """ Given a pile of coins of different values, determine the fewest number
+        of coins needed to meet a given amount total.
+    """
+    if total < 1:
+        return 0
+    coins.sort()
+    coins.reverse()
+    fewest = 0
+    for coin in coins:
+        if total <= 0:
+            break
+        buff = total // coin
+        fewest += buff
+        total -= (buff * coin)
+    if total != 0:
+        return -1
+    return fewest
